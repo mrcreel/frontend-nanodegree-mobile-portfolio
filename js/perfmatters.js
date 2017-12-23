6fe4bb0ea48aa1c9dba72eaed73707add0782dfc
mrcreel <michaelrcreel@gmail.com>
2017-12-23 14:55:45 -0600

    Move script to external file

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -1,1 +1,3 @@
+(function(w,g){w['GoogleAnalyticsObject']=g;
+      w[g]=w[g]||function(){(w[g].q=w[g].q||[]).push(arguments)};w[g].l=1*new Date();})(window,'ga');
 function logCRP(){var t=window.performance.timing,dcl=t.domContentLoadedEventStart-t.domLoading,complete=t.domComplete-t.domLoading;var stats=document.getElementById("crp-stats");stats.textContent='DCL: '+dcl+'ms, onload: '+complete+'ms';}window.addEventListener("load",function(event){logCRP();});
